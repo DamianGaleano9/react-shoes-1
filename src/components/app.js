41a32893c51c735a97650ed0d77ee0444013429c
Damian Galeano <dj.damian.b@hotmail.com>
2023-10-20 23:24:47 +0200

    Add Custom Function

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 
-import ProductContainer from './product-container';
+import ProductContainer from './products/product-container';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Shoes App</h1>
-        <ProductContainer/>
-        <ProductContainer/>
-        <ProductContainer/>
+   
         <ProductContainer/>
 
 
---
+++ src/components/products/product-container.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ProductItem from './product-item';
+
+export default class ProductContainer extends Component {
+    constructor() {
+        console.log('Render Constructor');
+        
+        super();
+    }
+
+
+    ProductItems() {
+        
+        
+        const data = ["aj1dior", "banned", "Shatters", "metallicpurple", "midnightnavy", "neutralgray", "obsidian","shadow", "shattered", "shatteredbackboard", "spiderman", "trophyroom"]
+            return data.map(item => {
+                return <ProductItem/>
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                {this.ProductItems()}
+                <ProductItem/>
+            </div>
+        )
+    }
+}
