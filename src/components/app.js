5e3b7c4ca0b04d3509f210f1d4ed5b30f54d8494
Damian Galeano <dj.damian.b@hotmail.com>
2023-10-20 20:07:48 +0200

    Create ProductContainer

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
+import ProductContainer from './product-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Bootstrap</h2>
+        <h1>Shoes App</h1>
+        <ProductContainer/>
+        <ProductContainer/>
+        <ProductContainer/>
+        <ProductContainer/>
+
+
       </div>
     );
   }
---
+++ src/components/product-container.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ProductContainer extends Component {
+    render() {
+        return (
+            <div>
+                Products Items 
+            </div>
+        )
+    }
+}
