87f5659d97f45232281129e771112fb4f8b252bb
Damian Galeano <dj.damian.b@hotmail.com>
2023-10-26 11:39:16 +0200

    Create base.scss for main

--- src/components/products/product-item.js
+++ src/components/products/product-item.js
@@ -6,19 +6,16 @@ export default function (props) {
 
     const { products_id, products_name, products_price, products_img } = props.item
 
+
+    const imgConverterUrl = products_img ? URL.createObjectURL(new Blob([new Uint8Array(products_img.data)])): "";
+
     return (
         <div>
             
+            <img src={imgConverterUrl} alt={products_name}/>
+            <div>{products_name}</div>
 
-            <div>
-                {products_name}
-
-            </div>
-
-            <div>
-                {products_price}
-
-            </div>
+            <div>{products_price}</div>
             <Link to={`/product/${products_id}`}>Link</Link>
         </div>
     );
