462c137ad51c1b79fcd3618ce7181006bfae05fd
Damian Galeano <dj.damian.b@hotmail.com>
2023-11-02 16:29:47 +0100

    Render the Images from s3

--- src/components/products/product-item.js
+++ src/components/products/product-item.js
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 
 export default function (props) {
 
-    const { products_id, products_name, products_price, products_img } = props.item
+    const { products_id, products_name, products_price, products_url } = props.item
 
 
-    const imgConverterUrl = products_img ? URL.createObjectURL(new Blob([new Uint8Array(products_img.data)])) : "";
+    // const imgConverterUrl = products_url ? URL.createObjectURL(new Blob([new Uint8Array(products_url.data)])) : "";
 
     return (
         <div>
             <div className="product-item-wrapper">
-                <img src={imgConverterUrl} alt={products_name} />
+                <img src={products_url} alt={products_name} />
                 <div>{products_name}</div>
 
                 <div>{products_price}</div>
