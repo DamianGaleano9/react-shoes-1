a6aabeb7e366bfbb0dccf503f944456d3b3fe43e
Damian Galeano <dj.damian.b@hotmail.com>
2023-11-04 16:56:19 +0100

    Building Form to login

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState} from "react";
+import { Axios } from "axios";
 
-export default function() {
+
+
+function Login() {
     return(
-        <div>
-            <h1>Login</h1>
+        <div className="container">
+            <div className="login-form">
+                <form>
+                    <h3>LOGIN</h3>
+                    <label htmlFor="username">Username</label>
+                    <input className="textInput" type="text"name="username" placeholer="Enter Your name" required/>
+                    <label htmlFor="password">Password</label>
+                    <input className="textInput" name="password" placeholder="Enter your password" required/>
+                    <input className="button" type="submit" value="login"/>
+                </form>
+                
+            </div>
+
+            <div className="login-form">
+                <form>
+                    <h3>REGISTER</h3>
+                    <label htmlFor="email">Email Address</label>
+                    <input className="textInput" type="text" name="email" placeholer="Enter Your email" required/>
+                    <label htmlFor="username">Username</label>
+                    <input className="textInput" type="username" name="username" placeholer="Enter Your Username" required/>
+                    <label htmlFor="password">Password</label>
+                    <input className="textInput" name="password" placeholder="Enter your password" required/>
+                    <input className="button" type="submit" value="Create an account"/>
+                </form>
+                
+            </div>
+            
         </div>
     )
 }
+
+export default Login;
