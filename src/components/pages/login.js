63dee63aa4e6dec3f219802f4a8365aaecffd3b0
Damian Galeano <dj.damian.b@hotmail.com>
2023-11-04 18:46:08 +0100

    Structure form

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,37 +1,37 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import { Axios } from "axios";
 
 
 
 function Login() {
-    return(
+    return (
         <div className="wrapper-form">
             <div className="login-form">
                 <form>
                     <h3>LOGIN</h3>
                     <label htmlFor="username">Username</label>
-                    <input className="textInput" type="text"name="username" placeholer="Enter Your name" required/>
+                    <input className="textInput" type="text" name="username" placeholer="Enter Your name" required />
                     <label htmlFor="password">Password</label>
-                    <input className="textInput" name="password" placeholder="Enter your password" required/>
-                    <input className="button" type="submit" value="login"/>
+                    <input className="textInput" name="password" placeholder="Enter your password" required />
+                    <input className="button" type="submit" value="login" />
                 </form>
-                
+
             </div>
 
             <div className="login-form">
                 <form>
                     <h3>REGISTER</h3>
                     <label htmlFor="email">Email Address</label>
-                    <input className="textInput" type="text" name="email" placeholer="Enter Your email" required/>
+                    <input className="textInput" type="text" name="email" placeholer="Enter Your email" required />
                     <label htmlFor="username">Username</label>
-                    <input className="textInput" type="username" name="username" placeholer="Enter Your Username" required/>
+                    <input className="textInput" type="username" name="username" placeholer="Enter Your Username" required />
                     <label htmlFor="password">Password</label>
-                    <input className="textInput" name="password" placeholder="Enter your password" required/>
-                    <input className="button" type="submit" value="Create an account"/>
+                    <input className="textInput" name="password" placeholder="Enter your password" required />
+                    <input className="button" type="submit" value="Create an account" />
                 </form>
-                
+
             </div>
-            
+
         </div>
     )
 }
