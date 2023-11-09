6f51f0d07fca015f8f21bdac14dcd49001fe583e
Damian Galeano <dj.damian.b@hotmail.com>
2023-11-09 12:02:41 +0100

    Refactoring to buyProduct include to context

--- src/components/context/data-context.js
+++ src/components/context/data-context.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { createContext, useState, useEffect} from "react";
+import { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
 
 export const dataContext = createContext();
 
 
-const DataProvider = ({children}) => {
+const DataProvider = ({ children }) => {
     const [data, setData] = useState([]); // data
     const [cart, setCart] = useState([]); // cart empty
 
@@ -18,7 +18,21 @@ const DataProvider = ({children}) => {
     }, [])
 
 
-    return <dataContext.Provider value={{ data, cart, setCart }}>{children}</dataContext.Provider>
+
+    const buyProducts = (product) => {
+        const productrepeat = cart.find((item) => item.products_id === product.products_id);
+
+        if (productrepeat) {
+            setCart(cart.map((item) => (item.id === product.products_id ? {
+                ...product, products_quanty:
+                    productrepeat.products_quanty + 1
+            } : item)));
+        } else {
+            setCart([...cart, product]);
+        }
+    }
+
+    return <dataContext.Provider value={{ data, cart, setCart, buyProducts }}>{children}</dataContext.Provider>
 };
 
 
