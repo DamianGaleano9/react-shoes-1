548d21127fc063fac1636d4ca504e7e58b4cca96
Damian Galeano <dj.damian.b@hotmail.com>
2023-11-10 15:36:37 +0100

    Handle Work

--- src/components/checkout-form.js
+++ src/components/checkout-form.js
@@ -5,11 +5,18 @@ import { Elements, CardElement} from "@stripe/react-stripe-js"
 const stripePromise = loadStripe("pk_test_51OAppxJ5RKKP5n8T5E24yShSaFPs0JO1SvcjuiJc4euHq0RByO8oxmQz8QJQ6YWgxeBIiMI441Ipb2VYaYMcaeJ100ESvq0LiJ");
 
 const CheckOutForm = ()  => {
-  return (
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('you can');
+  };
+  return  (
     <Elements stripe={stripePromise}>
 
-        <form className="checkout-form-container">
+        <form  onSubmit={handleSubmit} className="checkout-form-container">
           <CardElement/>
+          <button className="btn btn-primary">
+            Buy
+          </button>
         </form>
     </Elements>
   )
