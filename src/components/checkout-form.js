8bbbc28f697c1a5ec5af15e2944f25f90e9b5b96
Damian Galeano <dj.damian.b@hotmail.com>
2023-11-10 14:58:15 +0100

    Change arrow

--- src/components/checkout-form.js
+++ src/components/checkout-form.js
@@ -4,7 +4,7 @@ import { Elements, CardElement} from "@stripe/react-stripe-js"
 
 const stripePromise = loadStripe("pk_test_51OAppxJ5RKKP5n8T5E24yShSaFPs0JO1SvcjuiJc4euHq0RByO8oxmQz8QJQ6YWgxeBIiMI441Ipb2VYaYMcaeJ100ESvq0LiJ");
 
-function CheckOutForm ()  {
+const CheckOutForm = ()  => {
   return (
     <Elements stripe={stripePromise}>
 
